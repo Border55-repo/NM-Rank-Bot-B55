31624ddb321b9568a568d994e312402ef27dbefe
Border55-repo <juliannordli@gmail.com>
2026-09-12 11:57:09 +0200

    Add android/app/build.gradle.kts for NM-verktøy B55 v0.4.2

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "no.b55.nmrankbot"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "no.b55.nmrankbot"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 402
+        versionName = "0.4.2"
+    }
+}
